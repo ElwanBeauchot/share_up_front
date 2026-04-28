8c9a2dfdee756fbe4f15450758aba11f65d44915
Mattéo R <matt.yo95280@gmail.com>
2026-04-28 15:06:01 +0200

    lock screen orientation to portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'app.dart';
 import 'services/device_service.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([ // lock screen orientation to portrait
+    DeviceOrientation.portraitUp,
+  ]);
   await dotenv.load(fileName: ".env");
   try {
     final deviceService = DeviceService();
